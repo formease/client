48ab431a6c9bff9c3b7a9e9ae7bac2447b9bc1ad
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-03 19:48:35 +0530

    fix(route): register to auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const methods = require('../middleware/method');
 
-router.get('/register',methods([`GET`]), (req, res) => {
-    res.render('register');
+router.get('/auth',methods([`GET`]), (req, res) => {
+    res.render('auth');
 }
 );
 
