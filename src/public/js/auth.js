387eeb3ecf15d70229e7f413aa247c785e3c3003
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-04 14:49:35 +0530

    fix: CSP blocking scripts

--- src/public/js/auth.js
+++ src/public/js/auth.js
@@ -1,22 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { initializeApp } from "firebase/app";
 
 function FirebaseInit() {
-	return new Promise((res, rej) => {
-		const config = {
-            apiKey: "AIzaSyCtLctmVtf5LgHJv2y4h5eVx3h6DsM4KRY",
-            authDomain: "forms-server.firebaseapp.com",       
-            projectId: "forms-server",
-            storageBucket: "forms-server.appspot.com",
-            messagingSenderId: "265562888456",
-            appId: "1:265562888456:web:0ad6cfe8a44ef7af7234bd",
-		};
+  return new Promise((res, rej) => {
+    const config = {
+      apiKey: "AIzaSyCtLctmVtf5LgHJv2y4h5eVx3h6DsM4KRY",
+      authDomain: "forms-server.firebaseapp.com",
+      projectId: "forms-server",
+      storageBucket: "forms-server.appspot.com",
+      messagingSenderId: "265562888456",
+      appId: "1:265562888456:web:0ad6cfe8a44ef7af7234bd",
+    };
 
-		const app = initializeApp(config);
-		console.info('init done');
-		res();
-	});
+    const app = initializeApp(config);
+    res();
+  });
 }
 
 (async () => {
-await FirebaseInit()
-})()
+  await FirebaseInit();
+})();
