f98b64e0654371a5628f3c75c946718293f19e89
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-08 21:44:08 +0530

    Deploy for production

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
 @ECHO off
-ECHO Work in progress...
+ECHO Deploy for production
+TYPE .gitignore_deploy > .gitignore
+call git add .
+call git commit -m "Deploy for production"
+call git push deploy main
+TYPE .gitignore_original > .gitignore
 EXIT 
