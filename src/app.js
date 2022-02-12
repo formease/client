1d0fc7a4b091c715abce9220b7ceca05e66e8fed
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-12 19:17:28 +0530

    remove: .dccache

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(helmet({
 }))
 const corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
