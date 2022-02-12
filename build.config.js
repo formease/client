8e47eab619d8c5b0a579c401f57adcd56e7788b8
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-12 11:22:18 +0530

    fix: remove extended build scripts
    add functionality to remove deprecated built scripts in the public/dist dir

--- build.config.js
+++ build.config.js
@@ -4,26 +4,47 @@ const fs = require('fs')
 
 const logger = new Logger('FormEase', true, true)
 
-const files = fs.readdirSync('./src/public/js/')
-for (const file of files) {
-  if (file.endsWith('.js')) {
-    esbuild.build({
-      entryPoints: [`./src/public/js/${file}`],
-      outfile: `./src/public/dist/${file}`,
-      minify: true,
-      bundle: true,
-      platform: 'browser',
-      sourcemap: true,
-      define: {
-        'process.env.NODE_ENV': '"production"'
-      }
-    })
-      .catch((error) => {
-        logger.error(error)
-        process.exit(1)
+const builder = async () => {
+  const files = fs.readdirSync('./src/public/js/')
+  for (const file of files) {
+    if (file.endsWith('.js')) {
+      esbuild.build({
+        entryPoints: [`./src/public/js/${file}`],
+        outfile: `./src/public/dist/${file}`,
+        minify: true,
+        bundle: true,
+        platform: 'browser',
+        sourcemap: true,
+        define: {
+          'process.env.NODE_ENV': '"production"'
+        }
       })
-      .then((d) => {
-        logger.info(`Done | ${file} : ${JSON.stringify(d, null, 1)}`)
-      })
+        .catch((error) => {
+          logger.error(error)
+          process.exit(1)
+        })
+        .then((d) => {
+          logger.success(`Done - ${file}`)
+        })
+    }
   }
 }
+
+const checker = async () => {
+  logger.info('Checking for updates...')
+  const source = fs.readdirSync('./src/public/dist/')
+  const target = fs.readdirSync('./src/public/js/')
+  const diff = source.filter((file) => !target.includes(file) && file.endsWith('.js') && file !== 'prism.js')
+  if (diff.length > 0) {
+    for (const file of diff) {
+      fs.unlinkSync(`./src/public/dist/${file}`)
+      fs.unlinkSync(`./src/public/dist/${file}.map`)
+      logger.warn(`Deleted ${file}`)
+    }
+  }
+}
+
+(async () => {
+  await checker()
+  await builder()
+})()
