e6beb3742ee5c907dcc8bfcc2d86cc83f78c2c05
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-28 06:18:22 +0530

    fix: deploy port err

--- build/start/routes/dashboard.js
+++ build/start/routes/dashboard.js
@@ -4,18 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
-Route_1.default.get('/dashboard', async ({ view }) => {
+const Database_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Database"));
+Route_1.default.get('/dashboard', async ({ view, request }) => {
+    const data = await Database_1.default.from('users').where('uid', request.cookiesList().uid);
     return view.render('dashboard', {
-        data: [
-            {
-                projectName: 'hello',
-                projectDescription: 'world',
-            },
-            {
-                projectName: 'hello2',
-                projectDescription: 'world2',
-            },
-        ],
+        data: data,
     });
 });
 //# sourceMappingURL=dashboard.js.map
