330c756313b52ba0d9f0616047e39bcd2a2833b1
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-02-28 06:50:01 +0530

    feat(monitor): add pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  apps: [{
-    name: 'app',
-    script: './src/app.js',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: 'app',
+      script: './build/server.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }]
+  ],
 }
