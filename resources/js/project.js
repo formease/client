c63959b63e1a141de822a5972ae25aabfbf0d66c
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-04-17 13:26:29 +0530

    feat(dashboard): add copy to cb

--- resources/js/project.js
+++ resources/js/project.js
@@ -14,6 +14,22 @@ document.querySelector('[data-inp-mode-btn]').addEventListener('click', async (e
   document.getElementById('discord-webhook-link').type = 'password'
   e.target.innerText = 'Show link'
 })
+document.querySelector('[data-copy-btn]').addEventListener('click', () => {
+  const copyText = document.getElementById('google-link')
+  const clipboard = navigator.clipboard
+  clipboard.writeText(copyText.value).then(() => {
+    // fire a toast
+    Swal.fire({
+      toast: true,
+      icon: 'success',
+      title: 'Copied to clipboard!',
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+    })
+  })
+})
 document.querySelector('[data-projectDelete-btn]').addEventListener('click', async () => {
   const documentUrl = document.location.href
   const projectId = documentUrl.split('/')[4]
